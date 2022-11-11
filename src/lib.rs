c490f59428c315fb10f7749cebc114ed3a0ec7d4
YdrMaster <ydrml@hotmail.com>
2022-11-11 12:58:47 +0800

    docs: 补充文档
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+//! 为 qemu-system-riscv32/64 提供 SiFive 测试设备定义。
+
 #![no_std]
-#![deny(warnings)]
+#![deny(warnings, missing_docs)]
 
 use core::cell::UnsafeCell;
 
+/// SiFive 测试设备。
 #[repr(transparent)]
 pub struct SifiveTestDevice(UnsafeCell<u64>);
 
@@ -11,16 +14,19 @@ const PASS: u16 = 0x5555;
 const RESET: u16 = 0x7777;
 
 impl SifiveTestDevice {
+    /// 以 code 为错误码，退出进程。
     #[inline]
     pub fn fail(&self, code: u16) -> ! {
         self.write(FAIL as u64 | (code as u64) << 16)
     }
 
+    /// 以 0 为错误码，退出进程。
     #[inline]
     pub fn pass(&self) -> ! {
         self.write(PASS as _)
     }
 
+    /// 系统重启。
     #[inline]
     pub fn reset(&self) -> ! {
         self.write(RESET as _)
